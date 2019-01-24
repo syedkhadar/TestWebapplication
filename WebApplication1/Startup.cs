feec0406131bfa6fd7c6d4c53d686bae6da2e9c1
syedkhadar <khadar.syed@xerox.com>
2019-01-24 15:53:24 +0530

    chile added

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -10,7 +10,7 @@ namespace WebApplication1
 {
     public partial class Startup
     {
-        //Comments added branched
+        //Comments added child
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
